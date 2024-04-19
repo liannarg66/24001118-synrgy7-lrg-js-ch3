458b50aa05fdaf8bd0f664a1a56a8db3ee7dd9a7
liannarg66 <liannalrg66@gmail.com>
2024-04-19 23:46:52 +0700

    Challenge 3

---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,31 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  function bubbleSortbyDescendingly(cars, val){
+    let temp;
+    const n = cars.length
+
+    for(let i=0; i < n-1; i++) {
+        for(j=0; j < n-i-1; j++){
+            if(cars[j][val] < cars[j+1][val]) {
+                temp = cars[j];  
+                cars[j] = cars[j+1]; 
+                cars[j+1] = temp;
+           }
+       }
+    }
+
+  return cars
+}
+
+const finalresult = bubbleSortbyDescendingly(result, 'year');
+  
+  // Rubah code ini dengan array hasil sorting secara descending
+  return finalresult;
+}
